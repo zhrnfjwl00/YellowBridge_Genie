9585547bc4965a48e8a9be940a0037a9a1cdb1c1
wldus <wldus@LAPTOP-3JRPG9BN>
2021-06-23 10:04:04 +0900

    commit_0623_10:01_지연(게시글리스트 조회, 게시글 등록)

---
+++ src/main/java/com/kh/YellowBridge/support/model/dao/SupportDAO.java
@@ -0,0 +1,36 @@
+package com.kh.YellowBridge.support.model.dao;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.YellowBridge.common.PageInfo;
+import com.kh.YellowBridge.support.model.vo.Board;
+import com.kh.YellowBridge.support.model.vo.FileInfo;
+
+@Repository("sDAO")
+public class SupportDAO {
+
+	public int getListCount(SqlSessionTemplate sqlSession) {
+		return  sqlSession.selectOne("supportMapper.getListCount");
+	}
+
+	public ArrayList<Board> selectList(SqlSessionTemplate sqlSession, PageInfo pi) {
+		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+		return (ArrayList)sqlSession.selectList("supportMapper.selectList", null, rowBounds);
+	}
+
+	public int insertBoard(SqlSessionTemplate sqlSession, Board b) {
+		return sqlSession.insert("supportMapper.insertBoard", b);
+	}
+	
+	public int insertFile(SqlSessionTemplate sqlSession, FileInfo fi) {
+		return sqlSession.insert("supportMapper.insertFile", fi);
+	}
+
+
+}
