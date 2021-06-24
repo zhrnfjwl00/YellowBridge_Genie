9d7b274f9ac703451cad5a1653d4590bfe9337bd
saerin <saeriiin@naver.com>
2021-06-24 11:54:38 +0900

    6.24 입양관련 설정

---
+++ src/main/java/com/kh/YellowBridge/adoption/model/exception/AdoptionException.java
@@ -0,0 +1,6 @@
+package com.kh.YellowBridge.adoption.model.exception;
+
+public class AdoptionException extends RuntimeException {
+	public AdoptionException() {}
+	public AdoptionException(String msg) { super(msg); }
+}
